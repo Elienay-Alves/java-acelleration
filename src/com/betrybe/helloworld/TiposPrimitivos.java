7ba76179eedf9d3f0d39e1ad951c98f0e4dbae49
Elienay Alves <elienay8486@gmail.com>
2025-08-12 23:00:51 -0300

    feat(helloworld): add short variables and underscore-formatted numeric literals
    Introduced two new short variables in TiposPrimitivos: a regular short (`num`) and an underscore-formatted numeric literal (`formatadaComUndercore`) to demonstrate Java's ability to improve number readability. Updated output to display both values.

--- src/com/betrybe/helloworld/TiposPrimitivos.java
+++ src/com/betrybe/helloworld/TiposPrimitivos.java
@@ -4,5 +4,11 @@ public class TiposPrimitivos {
     public static void main(String[] args) {
         byte b = 9;
         System.out.println(b);
+
+        short num = 300;
+        short formatadaComUnderscore = 20_000; //variável
+
+        System.out.println(num);
+        System.out.println(formatadaComUnderscore); //20000
     }
 }
